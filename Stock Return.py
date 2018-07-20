4a609ac994e19b4096b468464323df9e96f4d64c
YiqingKevinDai <34862703+YiqingKevinDai@users.noreply.github.com>
2018-07-20 21:12:28 +0930

    Update and rename Daily Return.py to Stock Return.py

--- Stock Return.py
+++ Stock Return.py
@@ -1,6 +1,13 @@
+#Daily Return
 start = datetime.datetime(2012, 1, 1)
 end = datetime.datetime(2017, 1, 1)
 tesla = web.DataReader("TSLA", 'google', start, end)
 
 tesla['returns'] = (tesla['Close'] / tesla['Close'].shift(1) ) - 1
 tesla['returns'] = tesla['Close'].pct_change(1)
+
+# Cumulative Return
+
+tesla['Cumulative Return'] = (1 + tesla['returns']).cumprod()
+
+tesla['Cumulative Return'].plot(label='Tesla',figsize=(16,8),title='Cumulative Return')
